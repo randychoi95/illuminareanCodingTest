4955a0945e956b0b49870323e50971317dcf0ef0
randychoi <jhc130833@naver.com>
2023-10-04 21:47:45 +0900

    Initial Commit

---
+++ illuminareanCodingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  illuminareanCodingTest
+//
+//  Created by 최제환 on 10/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
