0caaae5c3a3b7f6845a590ef915590a7bd75f972
randychoi <jhc130833@naver.com>
2023-10-04 22:15:18 +0900

    - 화면에 보여질 ViewController 추가

--- illuminareanCodingTest/Application/AppDelegate.swift
+++ illuminareanCodingTest/Application/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        let githubSearchVC = GithubSearchViewController()
+        window?.rootViewController = githubSearchVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
